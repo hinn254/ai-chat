8efe1724fcac8fe7d8cec1691dd574800b4054e3
hinn254 <bennyhinnotieno@gmail.com>
2024-03-15 16:57:59 +0300

    Add ChatProvider to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import SideBar from "./components/sidebar/sidebar";
+import { ChatProvider } from "./context/chatContext";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,12 +20,14 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full bg-gray-50">
       <body className={`${inter.className} h-full overflow-hidden`}>
-        <div>
-          <SideBar />
-          <main className="pb-10 md:pl-72">
-            <div className="px-4 sm:px-6 md:px-8">{children}</div>
-          </main>
-        </div>
+        <ChatProvider>
+          <div>
+            <SideBar />
+            <main className="pb-10 md:pl-72">
+              <div className="px-4 sm:px-6 md:px-8">{children}</div>
+            </main>
+          </div>
+        </ChatProvider>
       </body>
     </html>
   );
