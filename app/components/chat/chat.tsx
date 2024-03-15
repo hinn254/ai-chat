ee05e04dcfe13c1098139226cfaee5f1cb2fa1ac
hinn254 <bennyhinnotieno@gmail.com>
2024-03-16 02:12:33 +0300

    Refactor chat component to display messages and show loading spinner

--- app/components/chat/chat.tsx
+++ app/components/chat/chat.tsx
@@ -1,17 +1,38 @@
-import { Prisma } from "@prisma/client";
+"use client";
+import { ChatContext } from "@/app/context/chatContext";
+import { useContext } from "react";
+import { ThreeDots } from "react-loader-spinner";
 import EmptyPlaceholder from "../placeholder";
 import Message from "./message";
 
-type ChatData = Prisma.ChatMessagesGetPayload<{}>;
-
-export default function Chat({ chatdata }: { chatdata?: ChatData[] }) {
+export default function Chat() {
+  const { specificChat, isSending } = useContext(ChatContext);
   return (
-    <div className="flex-[0.84] overflow-y-auto overflow-x-hidden md:flex-[0.98]">
-      {chatdata?.length === 0 && <EmptyPlaceholder />}
+    <div className="flex-[0.84] overflow-y-auto overflow-x-hidden pb-20 md:flex-[0.98]">
+      {!specificChat?.chatMessages?.length && <EmptyPlaceholder />}
 
-      {chatdata?.map((message) => (
-        <Message key={message.id} message={message} />
+      {specificChat?.chatMessages?.map((message) => (
+        <div key={message.id} className="last:mb-10">
+          <Message key={message.id} message={message} />
+        </div>
       ))}
+      {isSending && (
+        <ThreeDots
+          visible={true}
+          height="80"
+          width="80"
+          color="#497FE1"
+          radius="9"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{
+            display: "flex",
+            alignItems: "center",
+            marginLeft: "40px",
+            paddingBottom: "50px",
+          }}
+          wrapperClass=""
+        />
+      )}
     </div>
   );
 }
