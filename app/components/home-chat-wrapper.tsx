8c2035a39c989b4223b2dd45742756c5464e8383
hinn254 <bennyhinnotieno@gmail.com>
2024-03-16 02:11:03 +0300

    Added wrappers for the two diff flows: new chat and old chat

---
+++ app/components/home-chat-wrapper.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Chat from "@/app/components/chat/chat";
+import SearchInput from "@/app/components/chat/chat-input";
+import { ChartOptionsSkeleton } from "@/app/components/skeleton";
+import { ChatContext } from "@/app/context/chatContext";
+import { Prisma } from "@prisma/client";
+import { Suspense, useContext, useEffect } from "react";
+
+export function HomeWrapper({
+  data,
+}: {
+  data?: Prisma.ChatGetPayload<{
+    include: {
+      chatMessages: true;
+    };
+  }>;
+}) {
+  const { setSpecificChat } = useContext(ChatContext);
+
+  useEffect(() => {
+    setSpecificChat(data! ?? null);
+  }, [data, setSpecificChat]);
+
+  return (
+    <>
+      <Suspense fallback={<ChartOptionsSkeleton />}>
+        <Chat />
+      </Suspense>
+      <SearchInput />
+    </>
+  );
+}
